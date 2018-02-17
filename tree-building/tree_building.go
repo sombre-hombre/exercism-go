6ed3a9bb84c92032ba7df4e75fbdaa0724326087
Evgeniy Kolupaev <evgeniy.kolupaev@sravni.ru>
2018-02-17 22:58:25 +0300

    Tree building v2

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -6,159 +6,76 @@ import (
 	"sort"
 )
 
+type Record struct {
+	ID, Parent int
+}
+
 type Node struct {
 	ID       int
 	Children []*Node
 }
 
-func Build2(records []Record) (*Node, error) {
-	if len(records) == 0 {
-		return nil, nil
-	}
-
-	sort.Sort(byID(records))
-
-	if records[0].ID != 0 || records[0].Parent != 0 {
-		return nil, errors.New("Root not found")
-	}
-
-	if len(records) == 1 {
-		return &Node{ID: 0}, nil
-	}
-
-	children, er := getChildren(0, records[1:])
-	if er != nil {
-		return nil, er
-	}
-
-	for i := 1; i < len(records); i++ {
-		rec := records[i]
-		if rec.ID != i {
-			return nil, errors.New("Non-continuous or duplicated IDs")
-		}
-		if rec.ID > 0 && rec.ID <= rec.Parent {
-			return nil, errors.New("ID higher or equals than parent")
-		}
-	}
-
-	return &Node{ID: 0, Children: children}, nil
-}
-
-func getChildren(parent int, records []Record) ([]*Node, error) {
-	if len(records) == 0 {
-		return nil, nil
-	}
-
-	siblings := []Record{}
-	children := []Record{}
-	for _, r := range records {
-		if r.Parent == parent {
-			siblings = append(siblings, r)
-		} else {
-			children = append(children, r)
-		}
-	}
-
-	result := []*Node{}
-	for _, r := range siblings {
-		nodeChildren, err := getChildren(r.ID, children)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, &Node{ID: r.ID, Children: nodeChildren})
-	}
-
-	if len(result) == 0 {
-		return nil, nil
-	}
-
-	return result, nil
-}
-
-func removeItem(slice []Record, idx int) []Record {
-	slice[idx] = slice[len(slice)-1]
-	return slice[:len(slice)-1]
-}
-
 func Build(records []Record) (*Node, error) {
-	if len(records) == 0 {
+	recordsLength := len(records)
+	if recordsLength == 0 {
 		return nil, nil
 	}
-	root := &Node{}
-	todo := []*Node{root}
-	n := 1
-	for {
-		if len(todo) == 0 {
-			break
+
+	var nodes = make(map[int]*Node)
+	for _, record := range records {
+		if _, found := nodes[record.ID]; found {
+			return nil, fmt.Errorf("Duplicate ID %d found", record.ID)
 		}
-		newTodo := []*Node(nil)
-		for _, c := range todo {
-			for _, r := range records {
-				if r.Parent == c.ID {
-					if r.ID < c.ID {
-						return nil, errors.New("a")
-					} else if r.ID == c.ID {
-						if r.ID != 0 {
-							return nil, fmt.Errorf("b")
-						}
-					} else {
-						n++
-						nn := &Node{ID: r.ID}
-						newTodo = append(newTodo, nn)
-						switch len(c.Children) {
-						case 0:
-							c.Children = []*Node{nn}
-						case 1:
-							if c.Children[0].ID == r.ID {
-								return nil, errors.New("Duplicated ID")
-							} else if c.Children[0].ID < r.ID {
-								c.Children = []*Node{c.Children[0], nn}
-							} else {
-								c.Children = []*Node{nn, c.Children[0]}
-							}
-						default:
-						breakpoint:
-							for range []bool{false} {
-								for i, cc := range c.Children {
-									if cc.ID > r.ID {
-										a := make([]*Node, len(c.Children)+1)
-										copy(a, c.Children[:i])
-										copy(a[i+1:], c.Children[i:])
-										copy(a[i:i+1], []*Node{nn})
-										c.Children = a
-										break breakpoint
-									}
-								}
-								c.Children = append(c.Children, nn)
-							}
-						}
-					}
-				}
+		if record.ID == 0 {
+			if record.Parent != 0 {
+				return nil, errors.New("Root node has parent")
+			}
+		} else {
+			if record.ID == record.Parent {
+				return nil, fmt.Errorf("Self parent node %d", record.ID)
+			}
+			if record.ID <= record.Parent {
+				return nil, fmt.Errorf("Wrong link %d → %d", record.ID, record.Parent)
 			}
 		}
-		todo = newTodo
+
+		nodes[record.ID] = &Node{ID: record.ID}
 	}
-	if n != len(records) {
-		return nil, errors.New("c")
+
+	if _, found := nodes[0]; !found {
+		return nil, errors.New("Root node not found")
 	}
-	if err := chk(root, len(records)); err != nil {
-		return nil, err
+	if last, found := nodes[recordsLength-1]; !found || last.ID != recordsLength-1 {
+		return nil, errors.New("Non-continuous records")
 	}
-	return root, nil
+
+	for _, record := range records {
+		node := nodes[record.Parent]
+
+		if record.ID != 0 {
+			node.Children = append(node.Children, nodes[record.ID])
+		}
+	}
+
+	for i := 0; i < recordsLength; i++ {
+		if len(nodes[i].Children) > 1 {
+			sort.Sort(byID(nodes[i].Children))
+		}
+	}
+
+	return nodes[0], nil
 }
 
-func chk(n *Node, m int) (err error) {
-	if n.ID > m {
-		return fmt.Errorf("z")
-	} else if n.ID == m {
-		return fmt.Errorf("y")
-	} else {
-		for i := 0; i < len(n.Children); i++ {
-			err = chk(n.Children[i], m)
-			if err != nil {
-				return
-			}
-		}
-		return
-	}
+type byID []*Node
+
+func (arr byID) Len() int {
+	return len(arr)
+}
+
+func (arr byID) Swap(i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
+
+func (arr byID) Less(i, j int) bool {
+	return arr[i].ID < arr[j].ID
 }
