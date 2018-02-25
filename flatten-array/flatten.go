725d4515b64a0e9a8f43186a880703fde53c6f60
Evgeniy Kolupaev <evgeniy.kolupaev@sravni.ru>
2018-02-25 23:19:37 +0300

    flatten

---
+++ flatten-array/flatten.go
@@ -0,0 +1,28 @@
+package flatten
+
+// Flatten takes a nested list and returns a single flattened list
+// with all values except nil/null
+func Flatten(list interface{}) []interface{} {
+	result := make([]interface{}, 0)
+
+	array, ok := list.([]interface{})
+	if !ok {
+		if list != nil {
+			result = append(result, list)
+		}
+		return result
+	}
+
+	for _, element := range array {
+		switch el := element.(type) {
+		case []interface{}:
+			result = append(result, Flatten(el)...)
+		default:
+			if element != nil {
+				result = append(result, el)
+			}
+		}
+	}
+
+	return result
+}
