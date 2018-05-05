e09690b347ca92019496ca5e63eb547f38877609
Evgeniy Kolupaev <evgeniy.kolupaev@sravni.ru>
2018-05-05 16:36:07 +0300

    Largest Series Product v2

--- largest-series-product/lsproduct.go
+++ largest-series-product/lsproduct.go
@@ -6,9 +6,14 @@ import (
 	"strings"
 )
 
+// goos: windows
+// goarch: amd64
+// BenchmarkLargestSeriesProduct-8   	 1000000	      1702 ns/op	     104 B/op	       6 allocs/op
+// PASS
+
 // LargestSeriesProduct calculate the largest product for a
 // contiguous substring of digits of length n.
-func LargestSeriesProduct(digits string, span int) (int, error) {
+func LargestSeriesProduct(digits string, span int) (int64, error) {
 	if len(digits) < span {
 		return 0, errors.New("String is too short")
 	}
@@ -24,21 +29,30 @@ func LargestSeriesProduct(digits string, span int) (int, error) {
 	}
 
 	best := product(digits[:span])
-	//prev := best
-	current := 0
+	prevProd := best
+	currentProd := int64(0)
 	for i := 1; i <= len(digits)-span; i++ {
-		//first := toInt(digits[i])
-		last := toInt(digits[i+span-1])
-		if last == 0 {
+		// last digit in current window
+		lastDigit := toInt(digits[i+span-1])
+		// last digit before current window
+		prevDigit := toInt(digits[i-1])
+
+		if lastDigit == 0 {
+			// move window after zero
 			i += span - 1
 			continue
 		}
-		//current = (prev / toInt(digits[i-1])) * toInt(digits[i+span])
-		current = product(digits[i : i+span])
-		if best < current {
-			best = current
+
+		if prevDigit == 0 {
+			currentProd = product(digits[i : i+span])
+		} else {
+			currentProd = (prevProd / int64(prevDigit)) * int64(lastDigit)
 		}
-		//prev = current
+
+		if best < currentProd {
+			best = currentProd
+		}
+		prevProd = currentProd
 	}
 
 	return best, nil
@@ -48,14 +62,14 @@ func toInt(r byte) int {
 	return int(r - '0')
 }
 
-func product(wnd string) int {
+func product(wnd string) int64 {
 	if strings.Contains(wnd, "0") {
 		return 0
 	}
 
-	s := 1
+	s := int64(1)
 	for _, d := range wnd {
-		s *= int(d - '0')
+		s *= int64(d - '0')
 	}
 
 	return s
